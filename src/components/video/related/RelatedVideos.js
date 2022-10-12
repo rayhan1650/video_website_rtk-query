bfcfbd8f8734fcdf271a6916e45eadc071b21f9c
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-10-12 18:34:10 +0600

    related video api completed

--- src/components/video/related/RelatedVideos.js
+++ src/components/video/related/RelatedVideos.js
@@ -1,10 +1,44 @@
 import React from "react";
+import { useGetRelatedVideosQuery } from "../../../features/api/apiSlice";
+import Error from "../../ui/Error";
+import RelatedVideoLoader from "../../ui/loaders/RelatedVideoLoader";
 import RelatedVideo from "./RelatedVideo";
 
-const RelatedVideos = () => {
+const RelatedVideos = ({ id, title }) => {
+  const {
+    data: relatedVideos,
+    isError,
+    isLoading,
+  } = useGetRelatedVideosQuery({ id, title });
+
+  let content = null;
+  if (isLoading) {
+    content = (
+      <>
+        <RelatedVideoLoader />
+        <RelatedVideoLoader />
+        <RelatedVideoLoader />
+      </>
+    );
+  }
+
+  if (!isLoading && isError) {
+    content = <Error message="There is an error!" />;
+  }
+
+  if (!isLoading && !isError && relatedVideos?.length === 0) {
+    content = <Error message="No related video found!" />;
+  }
+
+  if (!isLoading && !isError && relatedVideos?.length > 0) {
+    content = relatedVideos.map((video) => (
+      <RelatedVideo key={video.id} video={video} />
+    ));
+  }
+
   return (
     <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
-      <RelatedVideo />
+      {content}
     </div>
   );
 };
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -12,7 +12,16 @@ export const apiSlice = createApi({
     getVideo: builder.query({
       query: (videoId) => `/videos2/${videoId}`,
     }),
+    getRelatedVideos: builder.query({
+      query: ({ id, title }) => {
+        const tags = title.split(" ");
+        const likes = tags.map((tag) => `title_like=${tag}`);
+        const queryString = `/videos2?${likes.join("&")}&_limit=4`;
+        return queryString;
+      },
+    }),
   }),
 });
 
-export const { useGetVideosQuery, useGetVideoQuery } = apiSlice;
+export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery } =
+  apiSlice;
